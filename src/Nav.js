dda22d9b002d1b0bcc88446b12a9ac0acc393245
Kelly Ford <kford@dnndev.com>
2018-04-24 17:00:16 -0700

    added Nav component for ToC; setup basic 2-column layout with Flexbox

---
+++ src/Nav.js
@@ -0,0 +1,43 @@
+
+import React from 'react'
+
+export default () => (
+  <nav>
+    <ul>
+      <li>Overview</li>
+      <li>Installation</li>
+      <li>Concepts
+        <ul>
+          <li>Architecture</li>
+          <li>Extensions
+            <ul>
+              <li>Modules</li>
+              <li>Language Packs</li>
+              <li>Providers</li>
+              <li>Libraries</li>
+              <li>Themes</li>
+            </ul>
+          </li>
+          <li>Users &amp; Roles
+            <ul>
+              <li>Creating a User</li>
+              <li>Creating a Role</li>
+              <li>Assigning a Role to a User</li>
+            </ul>
+          </li>
+          <li>Pages</li>
+          <li>Automated Processes</li>
+          <li>Database
+            <ul>
+              <li>DAL2</li>
+              <li>Peta Poco</li>
+              <li>Connections</li>
+              <li>Cache</li>
+            </ul>
+          </li>
+          <li>Web.config</li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+)
