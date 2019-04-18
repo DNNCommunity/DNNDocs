5dcf48ed2bff370c9c19ff6bb33a8748d8ebfa18
nvisionative <info@nvisionative.com>
2019-04-18 18:24:46 -0400

    Add Feedback Feature (#268)

--- templates/dnn-docs/conceptual.html.primary.js
+++ templates/dnn-docs/conceptual.html.primary.js
@@ -11,6 +11,7 @@ exports.transform = function (model) {
   model._disableToc = model._disableToc || !model._tocPath || (model._navPath === model._tocPath);
   model.docurl = model.docurl || common.getImproveTheDocHref(model, model._gitContribute, model._gitUrlPattern);
   model._dnneditions = common.beautifyDnnEditions(model.dnneditions);
+  model._feedbackurl = common.getFeedbackHref(model.docurl, model.title, model.uid);
 
   if (extension && extension.postTransform) {
     model = extension.postTransform(model);
