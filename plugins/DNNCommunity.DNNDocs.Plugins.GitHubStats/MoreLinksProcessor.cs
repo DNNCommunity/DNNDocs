48903ad9a40c4c46ce8f082040317a1f0c6f1640
Peter Donker <peter@bring2mind.net>
2020-06-03 17:04:54 +0200

    Add new processor to process "related-links" and "links"

---
+++ plugins/DNNCommunity.DNNDocs.Plugins.GitHubStats/MoreLinksProcessor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.DocAsCode.Plugins;
+using System.Composition;
+using System.Collections.Immutable;
+
+namespace DNNCommunity.DNNDocs.Plugins.GitHubStats
+{
+    [Export(nameof(MoreLinksProcessor), typeof(IPostProcessor))]
+    public class MoreLinksProcessor : IPostProcessor
+    {
+        public ImmutableDictionary<string, object> PrepareMetadata(ImmutableDictionary<string, object> metadata)
+        {
+            // TODO: add/remove/update property from global metadata
+            return metadata;
+        }
+
+        public Manifest Process(Manifest manifest, string outputFolder)
+        {
+            // TODO: add/remove/update all the files included in manifest
+            return manifest;
+        }
+    }
+}
